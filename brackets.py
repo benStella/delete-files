09cf6864950ec2582ee91a5be339e55cdda3304d
fangs <catfxngs@gmail.com>
2023-10-15 12:49:08 -0400

    adjust comments and function names

--- brackets.py
+++ brackets.py
@@ -5,22 +5,22 @@ import re
 directory = '/path/to/your/directory'
 
 # Define the words you want to search for within parentheses
-words_to_search = ['word1', 'word2', 'word3']
+word_filter = ['word1', 'word2', 'word3']
 
 # Function to filter and delete files
-def delete_files(directory, words_to_search):
+def delete_files(directory, word_filter):
     for filename in os.listdir(directory):
         if os.path.isfile(os.path.join(directory, filename)):
-            # Use regular expression to extract text within parentheses
+            # Extracts text within parentheses
             match = re.search(r'\((.*?)\)', filename)
             if match:
                 text_inside_parentheses = match.group(1)
-                # Check if any of the words to search are in the text inside parentheses
-                if not any(word in text_inside_parentheses for word in words_to_search):
+                # Check if filter matches text inside parentheses
+                if not any(word in text_inside_parentheses for word in word_filter):
                     file_path = os.path.join(directory, filename)
                     # >>> !!! Comment out below line to delete file !!! <<<
                     # os.remove(file_path)
                     print(f"Deleted: {filename}")
 
 # Call the function to delete files
-delete_files(directory, words_to_search)
+delete_files(directory, word_filter)
